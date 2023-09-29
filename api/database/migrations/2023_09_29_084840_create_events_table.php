60b75ca3109357a5161d85dfa10837de18f7b804
Dayo Oladapo <dayoesq@live.com>
2023-09-29 18:59:57 +0300

    Mixed: Added migration tables, models controllers

---
+++ api/database/migrations/2023_09_29_084840_create_events_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->enum('type', [
+                'daily',
+                'weekly',
+                'monthly',
+                'annually',
+                'bi_weekly',
+                'bi_monthly',
+                'bi_annual',
+                'miscellaneous'
+                ]
+            );
+            $table->unsignedBigInteger('created_by');
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->timestamp('starts_at');
+            $table->timestamp('ends_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
