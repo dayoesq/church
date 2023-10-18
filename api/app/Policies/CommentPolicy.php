6bd7f235e35224c43d3b98b15358cba8bee0b831
Dayo Oladapo <dayoesq@live.com>
2023-10-18 11:51:53 +0300

    New: Added implementation for comment policy and controller

--- api/app/Policies/CommentPolicy.php
+++ api/app/Policies/CommentPolicy.php
@@ -2,65 +2,40 @@
 
 namespace App\Policies;
 
-use App\Models\Comment;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
 class CommentPolicy
 {
     /**
      * Determine whether the user can view any models.
+     *
+     * @return bool
      */
-    public function viewAny(User $user): bool
+    public function viewAny(): bool
     {
-        //
+        return true;
     }
 
     /**
      * Determine whether the user can view the model.
+     *
+     * @param User $user
+     * @return bool
      */
-    public function view(User $user, Comment $comment): bool
+    public function view(User $user): bool
     {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, Comment $comment): bool
-    {
-        //
+        return $user->isAuthorized() && ($user->isSuper() || $user->isAdmin());
     }
 
     /**
      * Determine whether the user can delete the model.
+     *
+     * @param User $user
+     * @return bool
      */
-    public function delete(User $user, Comment $comment): bool
+    public function destroy(User $user): bool
     {
-        //
+        return $user->isAuthorized() && ($user->isSuper() || $user->isAdmin());
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Comment $comment): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Comment $comment): bool
-    {
-        //
-    }
 }
