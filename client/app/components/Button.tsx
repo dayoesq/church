df9d2bfd2d39a4594679663bd45eeeaedb652a6e
Dayo Oladapo <dayoesq@live.com>
2023-10-29 22:49:45 +0200

    Mixed: More styles for blog card

--- client/app/components/Button.tsx
+++ client/app/components/Button.tsx
@@ -1,33 +1,46 @@
-import * as React from 'react';
+import Link from 'next/link';
+import React from 'react';
 
 type ButtonProps = {
-    type: 'button' | 'submit' | 'reset' | undefined;
+    type?: 'button' | 'submit' | 'reset';
     className?: string;
-    style?: React.HTMLAttributes<HTMLButtonElement>;
-    children?: React.ReactNode;
+    style?: React.CSSProperties;
+    children: React.ReactNode;
+    linkType?: 'internal' | 'anchor';
+    href?: any;
+    // onClick?: React.DOMAttributes<HTMLButtonElement>;
 };
 
-export default function Buton({
-    type,
+export default function Button({
+    type = 'button',
     className,
     style,
-    children
-}: ButtonProps) {
-    return (
-        <button
-            disabled
-            type={type}
-            style={style}
-            className={`${className && className} ${
-                style && style
-            } py-2 px-4 border 
-            border-transparent 
-            text-sm font-medium 
-            rounded-md
-             text-white 
-             cursor-pointer`}
-        >
-            {children}
-        </button>
-    );
+    children,
+    linkType,
+    href
+}: // onClick
+ButtonProps) {
+    const commonButtonProps = {
+        type,
+        style,
+        className: `${className} ${style} py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white cursor-pointer tracking-wider`
+    };
+
+    if (linkType === 'anchor') {
+        return (
+            <a href={href} {...commonButtonProps}>
+                {children}
+            </a>
+        );
+    }
+
+    if (linkType === 'internal') {
+        return (
+            <Link href={href} {...commonButtonProps}>
+                {children}
+            </Link>
+        );
+    }
+
+    return <button {...commonButtonProps}>{children}</button>;
 }
